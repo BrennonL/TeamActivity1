d51a0a53e3049757a55efdf8959ed59eb5d5061b
BrennonL <lan18022@byui.edu>
2022-05-04 10:23:56 -0600

    First part of the projcet

--- Address.cs
+++ Address.cs
@@ -4,6 +4,16 @@ namespace Cse210_Unit02_Ta
 {
     public class Address
     {
+        public string _street;
+        public string _City;
+        public string _State;
+
+        public void DisplayMailingLabel()
+        {
+            Console.WriteLine($"Street:{_street}, ");
+            Console.Write($"City:{_City}, ");
+            Console.Write($"State:{_State},");
+        }
         
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Cse210_Unit02_Ta
     {
         public static void Main()
         {
-            Console.WriteLine("Welcome to the program.");
+            Address address1 = new Address();
+            address1._street = "312 New York Dr";
+            address1._City = "New York";
+            address1._State = "New York";
+
+            address1.DisplayMailingLabel()
+            {
+
+            }
         }
     }
 }
